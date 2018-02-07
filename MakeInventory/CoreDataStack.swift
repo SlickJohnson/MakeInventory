52db2307f5e1c5622e84a4f7ca2cf7b88dcda4a0
SlickJohnson <liwa.johnson@gmail.com>
2018-02-06 23:53:14 -0800

    Add date to cell in InventoryViewController tableview

--- MakeInventory/CoreDataStack.swift
+++ MakeInventory/CoreDataStack.swift
@@ -10,38 +10,48 @@ import Foundation
 import CoreData
 
 public final class CoreDataStack {
-    static let instance = CoreDataStack()
-    
-    private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(
-            name: "MakeInventory"
-        )
-        
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    lazy var viewContext: NSManagedObjectContext = {
-        let viewContext = persistentContainer.viewContext
-        return viewContext
-    }()
-    
-    lazy var privateContext: NSManagedObjectContext = {
-        return persistentContainer.newBackgroundContext()
-    }()
-    
-    func saveTo(context: NSManagedObjectContext) {
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+  static let instance = CoreDataStack()
+
+  private lazy var persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(
+      name: "MakeInventory"
+    )
+
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
+
+  lazy var viewContext: NSManagedObjectContext = {
+    let viewContext = persistentContainer.viewContext
+    return viewContext
+  }()
+
+  lazy var privateContext: NSManagedObjectContext = {
+    return persistentContainer.newBackgroundContext()
+  }()
+
+  func save(to context: NSManagedObjectContext) {
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
     }
+  }
+
+  func fetch(_ request: NSFetchRequest<Inventory>, from context: NSManagedObjectContext) -> [Inventory]? {
+    do {
+      return try context.fetch(request)
+    } catch {
+      let nserror = error as NSError
+      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    }
+  }
 }
+
--- MakeInventory/InventoriesViewController.swift
+++ MakeInventory/InventoriesViewController.swift
@@ -10,58 +10,68 @@ import UIKit
 import CoreData
 
 class InventoriesViewController: UIViewController {
-    let stack = CoreDataStack.instance
-    
-    @IBOutlet weak var tableView: UITableView!
-    var inventories = [Inventory]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
+  let stack = CoreDataStack.instance
+
+  @IBOutlet weak var tableView: UITableView!
+  var inventories = [Inventory]()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.dataSource = self
+    tableView.delegate = self
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    let fetch = NSFetchRequest<Inventory>(entityName: "Inventory")
+    do {
+      let result = try stack.viewContext.fetch(fetch)
+      self.inventories = result
+      self.tableView.reloadData()
+
+    } catch let error {
+      print(error)
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let fetch = NSFetchRequest<Inventory>(entityName: "Inventory")
-        do {
-            let result = try stack.viewContext.fetch(fetch)
-            self.inventories = result
-            self.tableView.reloadData()
-            
-        }catch let error {
-            print(error)
-        }
-    }
+  }
 }
 
 
 extension InventoriesViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return inventories.count
-    }
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return inventories.count
+  }
 }
 
 extension InventoriesViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "InventoryCell", for: indexPath)
-        
-        let item = inventories[indexPath.row]
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "x\(item.quantity)"
-        
-        return cell
-    }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "InventoryCell", for: indexPath)
+
+    let item = inventories[indexPath.row]
+
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy MMM dd"
+    cell.textLabel?.text = "\(dateFormatter.string(from: item.dateEntered!)): \(item.name!)"
+    cell.detailTextLabel?.text = "x\(item.quantity)"
+
+    return cell
+  }
 
   func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    let item = inventories[indexPath.row]
+
     let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
-      // delete item at indexPath
+      let context = self.stack.privateContext
+      let inv = self.stack.privateContext.object(with: item.objectID)
+
+      context.delete(inv)
+
+
+      self.stack.save(to: context)
       self.inventories.remove(at: indexPath.row)
       self.tableView.deleteRows(at: [indexPath], with: .automatic)
     }
